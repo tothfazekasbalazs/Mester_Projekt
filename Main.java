3ec0411474a8995164689a0aac682a7002bd3e55
AMte <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2025-01-07 10:00:08 +0100

    2.2 Körök létrehozása, kész játék.
    A játék kész, pontszámolás helyzetfrissítés, minden kör után enterrel indítható a következő.

--- Main.java
+++ Main.java
@@ -25,6 +25,42 @@ public class Main {
         mezok.add(new Mezo("Bónusz", "Lépsz egyet előre és kapsz 10 pontot!"));
 
 
-        
+        int kor = 1;
+        while (true) {
+            System.out.println("\nKör " + kor);
+            for (Jatekos jatekos : jatekosok) {
+                System.out.println(jatekos.getNev() + " következik!");
+                int lepes = random.nextInt(6) + 1;
+                System.out.println(jatekos.getNev() + " dobott: " + lepes);
+
+
+                jatekos.lep(lepes);
+                int mezoszam = jatekos.getHely() % mezok.size();
+                Mezo mez = mezok.get(mezoszam);
+                System.out.println(jatekos.getNev() + " a " + mez.getNev() + " mezőre lépett. " + mez.getHatas());
+
+                if (mez.getHatas().contains("két mezőt")) {
+                    jatekos.lep(-2);
+                } else if (mez.getHatas().contains("három lépést")) {
+                    jatekos.lep(-3);
+                } else if (mez.getHatas().contains("5 pontot")) {
+                    jatekos.hozzaadPont(5);
+                } else if (mez.getHatas().contains("10 pontot")) {
+                    jatekos.hozzaadPont(10);
+                }
+
+                System.out.println(jatekos.getNev() + " jelenlegi helye: " + jatekos.getHely() + " és pontja: " + jatekos.getPont());
+            }
+
+            for (Jatekos jatekos : jatekosok) {
+                if (jatekos.getHely() >= 30) {
+                    System.out.println(jatekos.getNev() + " nyert! Összegyűjtött pontok: " + jatekos.getPont());
+                    return;
+                }
+            }
+            System.out.println("\nNyomd meg az enter-t a folytatáshoz!");
+            scanner.nextLine();
+            kor++;
+        }
     }
 }
