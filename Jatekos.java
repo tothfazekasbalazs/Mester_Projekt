b5730030a1e5b0559488739d2f7e2f9e82c9b9d0
KovacsGergo1 <102990597+KovacsGergo1@users.noreply.github.com>
2025-01-07 08:41:06 +0100

    1.0 Jatekos Getter Setter
    Jatekos Java megcsinálása, getterekkel és setterekkel

---
+++ Jatekos.java
@@ -0,0 +1,35 @@
+public class Jatekos {
+    private String nev;
+    private int hely;
+    private int pont;
+
+    public Jatekos(String nev) {
+        setNev(nev);
+        setHely(0);
+        setPont(0);
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public int getHely() {
+        return hely;
+    }
+
+    public void setHely(int hely) {
+        this.hely = hely;
+    }
+
+    public int getPont() {
+        return pont;
+    }
+
+    public void setPont(int pont) {
+        this.pont = pont;
+    }
+}
